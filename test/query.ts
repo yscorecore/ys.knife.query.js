1657545c76c804c6e6e7ab4385dfee83aa644951
pengbo.yang <obpt@sina.com>
2024-08-13 08:02:26 +0800

    add some types

---
+++ test/query.ts
@@ -0,0 +1,45 @@
+import { QueryBuilder as QB, Query ,query} from "../src/query";
+import { PagedList } from "../src/pagedlist";
+import { FilterInfo } from "../src/filter";
+import { OrderByInfo } from "../src/orderby";
+import {path} from "../src/path";
+import { Operator } from "../src/operator";
+
+interface User {
+    name: string;
+    age: number;
+    email?: string; // 可选属性
+    address: {
+        city: string;
+        street: string
+    }
+}
+
+async function fetchUser(query: Query): Promise<PagedList<User>> {
+    return {
+        limit: 10,
+        offset: 0,
+        hasNext: false,
+        totalCount: 5,
+        agg: {},
+        items: [
+            {
+                name: 'zhangsan',
+                age: 10,
+                address: {
+                    city: 'xian',
+                    street: 'yanta'
+                }
+            }
+        ]
+    };
+}
+
+
+test('adds 1 + 2 to equal 3', () => {
+ 
+    query<User>()
+    .where("name",'abc',Operator.Equals)
+    
+    .build();
+  });
