c162bb4db3e0f19fe8e5005bc3331f225ce17ec6
yangsi <obpt@sina.com>
2026-01-19 09:00:09 +0800

    support abort signal

--- src/api.ts
+++ src/api.ts
@@ -7,23 +7,19 @@ import { SelectInfo } from "./select";
 import { OrderByInfo } from "./orderby";
 import { DeepKeys } from "./type";
 import { DataOverflowError } from "./errors";
-export type PageFunc<T> = (req: PageReq) => Promise<PagedList<T>>;
-export type PageFunc2<T> = (offset?: number, limit?: number, agg?: string | null, filter?: string | null, orderBy?: string | null, select?: string | null, distinct?: boolean) => Promise<PagedList<T>>;
-
-export function $page<T>(func: PageFunc2<T>): PageFunc<T> {
-    return (req: PageReq) => func(req.offset, req.limit, req.agg, req.filter, req.orderBy, req.select, req.distinct);
-}
+export type PageFunc<T> = (req: PageReq, signal?: AbortSignal) => Promise<PagedList<T>>;
 
 export interface IdEntity {
     id?: string | number
 }
 
-export async function findBy<T>(func: PageFunc<T>, key: keyof T, val: string | number | boolean | bigint): Promise<T | null> {
+
+export async function findBy<T>(func: PageFunc<T>, key: keyof T, val: string | number | boolean | bigint, signal?: AbortSignal): Promise<T | null> {
     const res = await func({
         limit: 1,
         offset: 0,
         filter: filter<T>(key, Operator.Equals, con(val)).toString(),
-    });
+    }, signal);
     if (res.items.length > 0) {
         return res.items[0];
     } else {
@@ -31,29 +27,29 @@ export async function findBy<T>(func: PageFunc<T>, key: keyof T, val: string | n
     }
 }
 
-export async function findById<T extends IdEntity>(func: PageFunc<T>, id: number | string): Promise<T | null> {
-    return findBy(func, "id", id);
+export async function findById<T extends IdEntity>(func: PageFunc<T>, id: number | string, signal?: AbortSignal): Promise<T | null> {
+    return findBy(func, "id", id, signal);
 }
-export async function count<T>(func: PageFunc<T>, filter?: FilterInfo | string | null): Promise<number> {
+export async function count<T>(func: PageFunc<T>, filter?: FilterInfo | string | null, signal?: AbortSignal): Promise<number> {
     const res = await func({
         limit: 0,
         offset: 0,
         filter: filter?.toString(),
-    });
+    }, signal);
     return res.totalCount;
 }
-export async function distinctCount<T>(func: PageFunc<T>, select: SelectInfo | string, filter?: FilterInfo | string | null): Promise<number> {
+export async function distinctCount<T>(func: PageFunc<T>, select: SelectInfo | string, filter?: FilterInfo | string | null, signal?: AbortSignal): Promise<number> {
     const res = await func({
         limit: 0,
         offset: 0,
         filter: filter?.toString(),
         select: select.toString(),
         distinct: true
-    });
+    }, signal);
     return res.totalCount;
 }
 //
-export async function distinctList<T>(func: PageFunc<T>, arg: { select: SelectInfo | string, filter?: FilterInfo | string | null, orderBy?: OrderByInfo | string | null, maxPageSize?: number, throwIfOverflow?: boolean }): Promise<T[]> {
+export async function distinctList<T>(func: PageFunc<T>, arg: { select: SelectInfo | string, filter?: FilterInfo | string | null, orderBy?: OrderByInfo | string | null, maxPageSize?: number, throwIfOverflow?: boolean }, signal?: AbortSignal): Promise<T[]> {
     const res = await func({
         limit: arg.maxPageSize ?? queryConfig.maxLimit,
         offset: 0,
@@ -61,20 +57,20 @@ export async function distinctList<T>(func: PageFunc<T>, arg: { select: SelectIn
         select: arg.select.toString(),
         orderBy: arg.orderBy?.toString(),
         distinct: true
-    });
+    }, signal);
     if (res.hasNext && (arg.throwIfOverflow ?? true)) {
         throw new DataOverflowError(`The result data of distinctList is lost. Total: ${res.totalCount}, Max page size: ${res.limit}`);
     }
     return res.items;
 }
-export async function asList<T>(func: PageFunc<T>, arg?: { filter?: FilterInfo | string | null, orderBy?: OrderByInfo | string | null, select?: SelectInfo | string | null, maxPageSize?: number, throwIfOverflow?: boolean }): Promise<T[]> {
+export async function asList<T>(func: PageFunc<T>, arg?: { filter?: FilterInfo | string | null, orderBy?: OrderByInfo | string | null, select?: SelectInfo | string | null, maxPageSize?: number, throwIfOverflow?: boolean }, signal?: AbortSignal): Promise<T[]> {
     const res = await func({
         limit: arg?.maxPageSize ?? queryConfig.maxLimit,
         offset: 0,
         filter: arg?.filter?.toString(),
         select: arg?.select?.toString(),
         orderBy: arg?.orderBy?.toString(),
-    });
+    }, signal);
     if (res.hasNext && (arg?.throwIfOverflow ?? true)) {
         throw new DataOverflowError(`The result data of asList is lost. Total: ${res.totalCount}, Max page size: ${res.limit}`);
     }
@@ -101,7 +97,7 @@ export function queryPage<T>(func: PageFunc<T>, arg: {
     });
 }
 
-export async function loadAll<T>(func: PageFunc<T>, arg?: { filter?: FilterInfo | string | null, orderBy?: OrderByInfo | string | null, select?: SelectInfo | string | null, distinct?: boolean, maxPageSize?: number }): Promise<T[]> {
+export async function loadAll<T>(func: PageFunc<T>, arg?: { filter?: FilterInfo | string | null, orderBy?: OrderByInfo | string | null, select?: SelectInfo | string | null, distinct?: boolean, maxPageSize?: number }, signal?: AbortSignal): Promise<T[]> {
     let resArray: T[] = [];
     let offset = 0;
     const args = {
@@ -112,46 +108,49 @@ export async function loadAll<T>(func: PageFunc<T>, arg?: { filter?: FilterInfo 
         distinct: arg?.distinct ?? false,
     }
     while (true) {
+        if (signal?.aborted) {
+            break;
+        }
         const res = await func({
             ...args,
             offset: offset,
-        });
+        }, signal);
         resArray = resArray.concat(res.items);
         offset += args.limit;
-        if (res.items.length == 0 || !res.hasNext) {
+        if (res.items.length == 0 || !res.hasNext || signal?.aborted) {
             break;
         }
     }
     return resArray;
 }
-export async function aggValue<T>(func: PageFunc<T>, agg: AggInfo | string, filter?: FilterInfo | string | null): Promise<AggResult> {
+export async function aggValue<T>(func: PageFunc<T>, agg: AggInfo | string, filter?: FilterInfo | string | null, signal?: AbortSignal): Promise<AggResult> {
     const res = await func({
         limit: 0,
         offset: 0,
         filter: filter?.toString(),
         agg: agg.toString()
-    });
+    }, signal);
     return res.aggs as AggResult;
 }
-export async function aggProp<T>(func: PageFunc<T>, prop: DeepKeys<T>, aggType: AggType = AggType.Sum, filter?: FilterInfo | string | null): Promise<number> {
+export async function aggProp<T>(func: PageFunc<T>, prop: DeepKeys<T>, aggType: AggType = AggType.Sum, filter?: FilterInfo | string | null, signal?: AbortSignal): Promise<number> {
     const tempAggKey = "__tg0";
     const res = await func({
         limit: 0,
         offset: 0,
         filter: filter?.toString(),
         agg: agg(prop, aggType, tempAggKey).toString()
-    });
+    }, signal);
     return Number(res.aggs?.[tempAggKey])
 }
-export async function sumProp<T>(func: PageFunc<T>, prop: DeepKeys<T>, filter?: FilterInfo | string | null): Promise<number> {
-    return aggProp(func, prop, AggType.Sum, filter);
+export async function sumProp<T>(func: PageFunc<T>, prop: DeepKeys<T>, filter?: FilterInfo | string | null, signal?: AbortSignal): Promise<number> {
+    return aggProp(func, prop, AggType.Sum, filter, signal);
 }
-export async function maxProp<T>(func: PageFunc<T>, prop: DeepKeys<T>, filter?: FilterInfo | string | null): Promise<number> {
-    return aggProp(func, prop, AggType.Max, filter);
+export async function maxProp<T>(func: PageFunc<T>, prop: DeepKeys<T>, filter?: FilterInfo | string | null, signal?: AbortSignal): Promise<number> {
+    return aggProp(func, prop, AggType.Max, filter, signal);
 }
-export async function minProp<T>(func: PageFunc<T>, prop: DeepKeys<T>, filter?: FilterInfo | string | null): Promise<number> {
-    return aggProp(func, prop, AggType.Min, filter);
+export async function minProp<T>(func: PageFunc<T>, prop: DeepKeys<T>, filter?: FilterInfo | string | null, signal?: AbortSignal): Promise<number> {
+    return aggProp(func, prop, AggType.Min, filter, signal);
 }
-export async function avgProp<T>(func: PageFunc<T>, prop: DeepKeys<T>, filter?: FilterInfo | string | null): Promise<number> {
-    return aggProp(func, prop, AggType.Avg, filter);
+export async function avgProp<T>(func: PageFunc<T>, prop: DeepKeys<T>, filter?: FilterInfo | string | null, signal?: AbortSignal): Promise<number> {
+    return aggProp(func, prop, AggType.Avg, filter, signal);
 }
